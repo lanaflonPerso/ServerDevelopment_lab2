1413c40753491202aaa3517d824e68cd55651d7b
magnus-1 <magnus-1@users.noreply.github.com>
2016-11-02 15:50:24 +0100

    Initial commit

---
+++ src/MyPack/Myname.java
@@ -0,0 +1,21 @@
+package MyPack;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ * Created by o_0 on 2016-11-02.
+ */
+@SessionScoped
+@ManagedBean
+public class Myname {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
