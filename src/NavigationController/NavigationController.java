dbdd25d35f943cff965f45ef084e4c96d0ed89f8
sirprice <carljohan.dahlman@gmail.com>
2016-11-03 17:45:45 +0100

    home med tabbar

--- src/NavigationController/NavigationController.java
+++ src/NavigationController/NavigationController.java
@@ -10,18 +10,18 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class NavigationController {
     public String moveToStart() {
-        return "Destination1.jsf";
+        return "Destination1";
     }
 
     public String moveToPage() {
-        return "Destination2.jsf";
+        return "Destination2";
     }
 
     public String moveToSelection(String id) {
         if(id.equals("1")) {
-            return "Destination1.jsf";
+            return "Destination1";
         }else if(id.equals("0")){
-            return "index.jsf";
+            return "index";
         }
         return "Destination2.jsf";
     }
