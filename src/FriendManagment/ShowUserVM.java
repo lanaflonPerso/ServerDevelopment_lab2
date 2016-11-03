588097e9198f42a12b97c464299592aa20c40793
magnus-1 <magnus-1@users.noreply.github.com>
2016-11-03 16:21:14 +0100

    First stab

---
+++ src/FriendManagment/ShowUserVM.java
@@ -0,0 +1,36 @@
+package FriendManagment;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+/**
+ * Created by o_0 on 2016-11-03.
+ */
+@SessionScoped
+@ManagedBean
+public class ShowUserVM implements Serializable {
+    private String name;
+
+    public ShowUserVM() {
+    }
+
+    public ShowUserVM(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "ShowUserVM{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
