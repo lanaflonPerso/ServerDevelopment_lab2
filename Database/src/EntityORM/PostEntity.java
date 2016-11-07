8bff6f174d2fb8edde84334398b0466c51db1446
magnus-1 <magnus-1@users.noreply.github.com>
2016-11-07 11:50:59 +0100

    Database and project structure

---
+++ Database/src/EntityORM/PostEntity.java
@@ -0,0 +1,90 @@
+package EntityORM;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+/**
+ * Created by o_0 on 2016-11-07.
+ */
+@Entity
+//@Table(name = "Post", schema = "CommunityDB", catalog = "")
+public class PostEntity {
+    private int id;
+    private String subject;
+    private String messageBody;
+    private Timestamp timestamp;
+    private ProfilEntity postedTo;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "Subject")
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Basic
+    @Column(name = "MessageBody")
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    @Basic
+    @Column(name = "Timestamp")
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PostEntity that = (PostEntity) o;
+
+        if (id != that.id) return false;
+        if (subject != null ? !subject.equals(that.subject) : that.subject != null) return false;
+        if (messageBody != null ? !messageBody.equals(that.messageBody) : that.messageBody != null) return false;
+        if (timestamp != null ? !timestamp.equals(that.timestamp) : that.timestamp != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (subject != null ? subject.hashCode() : 0);
+        result = 31 * result + (messageBody != null ? messageBody.hashCode() : 0);
+        result = 31 * result + (timestamp != null ? timestamp.hashCode() : 0);
+        return result;
+    }
+
+    @ManyToOne(optional = false)
+    public ProfilEntity getPostedTo() {
+        return postedTo;
+    }
+
+    public void setPostedTo(ProfilEntity postedTo) {
+        this.postedTo = postedTo;
+    }
+}
