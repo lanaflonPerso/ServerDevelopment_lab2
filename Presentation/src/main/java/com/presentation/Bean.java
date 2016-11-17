28ea89f9a5e5d2e4ef807936f57d9650a5c0ca8b
sirprice <carljohan.dahlman@gmail.com>
2016-11-17 15:13:55 +0100

    new home

---
+++ Presentation/src/main/java/com/presentation/Bean.java
@@ -0,0 +1,29 @@
+package com.presentation;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+/**
+ * Created by cj on 2016-11-17.
+ */
+@ManagedBean
+@ViewScoped
+public class Bean implements Serializable {
+
+    private String page;
+
+    @PostConstruct
+    public void init() {
+        page = "GetNewFriends"; //  Default include.
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+}
