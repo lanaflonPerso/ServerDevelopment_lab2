aa23cedae4af03f1ca53cfaf74176bce01f5c972
magnus-1 <magnus-1@users.noreply.github.com>
2016-11-21 18:08:05 +0100

    chat progress

--- Presentation/src/main/java/chat/ChatData.java
+++ Presentation/src/main/java/chat/ChatData.java
@@ -1,11 +1,31 @@
 package chat;
 
-import org.primefaces.push.annotation.PushEndpoint;
+import org.primefaces.push.EventBus;
+import org.primefaces.push.RemoteEndpoint;
+import org.primefaces.push.annotation.*;
 
 /**
  * Created by o_0 on 2016-11-21.
  */
-@PushEndpoint("{room}")
+@PushEndpoint("/{room}/{user}")
+@Singleton
 public class ChatData {
+
+    @PathParam("room")
+    private  String theRoom;
+
+    @PathParam("user")
+    private String theUser;
+
+    @OnOpen
+    public void onOpen(RemoteEndpoint r, EventBus eventBus)
+    {
+
+    }
+    @OnClose
+    public void onClose(RemoteEndpoint r, EventBus eventBus)
+    {
+
+    }
 
 }
--- Presentation/src/main/java/chat/ChatWindow.java
+++ Presentation/src/main/java/chat/ChatWindow.java
@@ -1,10 +1,12 @@
 package chat;
 
+import org.primefaces.context.RequestContext;
 import org.primefaces.push.EventBus;
 import org.primefaces.push.EventBusFactory;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.util.ArrayList;
 
 /**
@@ -15,7 +17,28 @@ import java.util.ArrayList;
 public class ChatWindow {
     private final EventBus eventBus = EventBusFactory.getDefault().eventBus();
 
-//    private static final String CHANNEL = "/{room}"/;
+    private static final String CHANNEL = "/{room}/";
+    private String theMessage;
+    private boolean isLoggedin = false;
+
+    public String getTheMessage() {
+        return theMessage;
+    }
+
+    public boolean isLoggedin() {
+        return isLoggedin;
+    }
+
+    public void setTheMessage(String theMessage) {
+        this.theMessage = theMessage;
+    }
+
+    public void login() {
+        // this starts the socket connection command in the page,
+        RequestContext.getCurrentInstance().execute("PF('subscriber').connect('/" + "name22" + "')");
+        isLoggedin = true;
+
+    }
 
     public ArrayList<String> getTextLists() {
         return textLists;
@@ -27,6 +50,8 @@ public class ChatWindow {
 
     private ArrayList<String> textLists = new ArrayList<String>();
     public void postMsg() {
+        eventBus.publish(CHANNEL + "u22", "message text");
+        //FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().get();
         //eventBus.publish()
     }
 }
