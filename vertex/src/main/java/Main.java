a61b0bf7bb2bbaf8cf66696c071544dac5a31065
sirprice <carljohan.dahlman@gmail.com>
2016-12-05 14:24:09 +0100

    vertex chatten

---
+++ vertex/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+
+/**
+ * Created by cj on 2016-12-05.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        DeploymentOptions options = new DeploymentOptions().setWorker(true);
+        Vertx.vertx().deployVerticle(new Server(),options);
+
+    }
+}
---
+++ vertex/src/main/java/Server.java
@@ -0,0 +1,43 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+/**
+ * Created by cj on 2016-12-05.
+ */
+public class Server extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> fut) throws Exception {
+        Router router = Router.router(vertx);
+
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/html").end("<h1>Hello from my first Vert.x 3 application</h1>");
+        });
+
+        router.route("/users").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/html").end("Display all the users!");
+        });
+
+
+
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        }
+                );
+
+    }
+}
